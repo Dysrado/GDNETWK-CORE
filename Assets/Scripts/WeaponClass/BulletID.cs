126d4429a79e791b0bf2ffe919d48a5aad3793c3
Dysrado <rajjardin@gmail.com>
2023-07-10 20:42:27 +0800

    added bullet id

--- Assets/Scripts/WeaponClass/BulletID.cs
+++ Assets/Scripts/WeaponClass/BulletID.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using System;
+using System.Reflection;
 
 public class BulletID : MonoBehaviour
 {
+    private readonly Color[] colors = { Color.red, Color.blue, Color.green, Color.yellow, Color.black, Color.white, Color.cyan, Color.gray };
+
     protected int ownerId;
     // Start is called before the first frame update
+    
     public void Initialize(int ID)
     {
         ownerId = ID;
         Debug.Log("client id:" + ownerId);
+        GetComponent<MeshRenderer>().material.color = colors[ownerId % colors.Length];
+
     }
 
     // Update is called once per frame
